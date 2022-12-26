5042981e858880bd2fa2540971031f3d1224ca5d
Burak Bahar <burak.bahar@metu.edu.tr>
2022-12-26 21:58:20 +0300

    modified get sql query to take input from user, then get query from Ml and the data from database

--- backend/nlq/views.py
+++ backend/nlq/views.py
@@ -1,33 +1,55 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-
+import json
+import requests
+from utils import connect_sql_db
 # Create your views here.
 
 
-def hello(response):
+def hello(request):
     return HttpResponse("Hello")
 
 
-def get_sql_query(response):
-    #Todo: Calculate query
+def get_sql_query(request):
+    if request.method == "POST":
+        return JsonResponse(
+            {"status": "error", "message": "Wrong method"}, status=405
+        )
 
-    query = "SELECT * FROM X WHERE Y = Z"
+    data = json.loads(request.body)
+    input_sentence = data.get("input")
+    port = data.get("port")
+    server = data.get("server")
+    database = data.get("database")
+    username = data.get("username")
+    password = data.get("password")
 
-    #Todo: get data
+    database_info = "| concert_singer | stadium : stadium_id, location, name, capacity, highest, lowest, average | " \
+                    "singer : singer_id, name, country, song_name, song_release_year, age, is_male | concert : " \
+                    "concert_id, concert_name, theme, stadium_id, year | singer_in_concert : concert_id, singer_id"
+    if input_sentence is None:
+        return JsonResponse(
+            {"status": "error", "message": "Input is required"}, status=404)
 
-    data = {
-        "header": ["Name", "Surname", "Age", "Department", "Year"],
-        "data": [
-            ["Ali", "Yılmaz", "22", "Computer Engineering", "4"],
-            ["Mehmet", "Türkmen", "21", "Mechanical Engineering", "3"],
-            ["Öykü", "Yıldız", "23", "Computer Engineering", "4"],
-            ["Ekrem", "Çetin", "19", "Electric and Electronic Engineering", "1"],
-            ["Gizem", "Yıldırım", "22", "Biology", "4"],
-        ]
-    }
+    r = requests.post("https://a434edb2-d402-43d8.gradio.live/run/predict", json={ #url will be changed according to colab link
+        "data": {"sentence": input_sentence,        #these can be changed back to hello world if they give errors
+                 "db_info": database_info}}).json()
 
+    if r.status_code != 200:
+        return JsonResponse({
+            "status": "error",
+            "message": r.text}, status=r.status_code)
+
+    query = r.get("data")[0]
+
+    engine = connect_sql_db('postgresql', username, password, server, database)
+    cursor = engine.raw_connection().cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    engine.close()
 
     return JsonResponse({
         "query": query,
-        "data": data
+        "data": result
     })
