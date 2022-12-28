38662ecc9c2cae99447ab7f9f7232acda566ac26
CELAL KEKEC <e2237618@ceng.metu.edu.tr>
2022-12-28 10:57:37 +0000

    Upload New File

---
+++ frontend/lib/data.dart
@@ -0,0 +1,386 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:nl2ssql/main.dart';
+import 'package:responsive_table/responsive_table.dart';
+
+class DataPage extends StatefulWidget {
+  DataPage({Key? key}) : super(key: key);
+  @override
+  _DataPageState createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  late List<DatatableHeader> _headers;
+  List<Map<String, dynamic>> datajson = [
+    {
+      "rengi": "beyaz",
+      "userID": "0",
+      "userName": "celal",
+      "password": "Esyc",
+      "lvl": "admin",
+      "birimID": "1"
+    },
+    {
+      "rengi": "beyaz",
+      "userID": "0",
+      "userName": "songül",
+      "password": "Esyc",
+      "lvl": "admin",
+      "birimID": "1"
+    },
+    {
+      "rengi": "beyaz",
+      "userID": "0",
+      "userName": "ertuğrul",
+      "password": "Esy",
+      "lvl": "admin",
+      "birimID": "1"
+    },
+    {
+      "rengi": "beyaz",
+      "userID": "5",
+      "userName": "yaren",
+      "password": "Esyc",
+      "lvl": "admin",
+      "birimID": "1"
+    }
+  ];
+  List<int> _perPages = [10, 20, 50, 100];
+  int? _total;
+  int? _currentPerPage = 10;
+  List<bool>? _expanded;
+  String? _searchKey = "id";
+
+  int _currentPage = 1;
+  bool _isSearch = false;
+  List<Map<String, dynamic>> _sourceOriginal = [];
+  List<Map<String, dynamic>> _sourceFiltered = [];
+  List<Map<String, dynamic>> _source = [];
+  List<Map<String, dynamic>> _selecteds = [];
+  // ignore: unused_field
+  String _selectableKey = "id";
+
+  String? _sortColumn;
+  bool _sortAscending = true;
+  bool _isLoading = true;
+  bool _showSelect = true;
+  var random = new Random();
+
+  List<Map<String, dynamic>> _generateData({int n: 100}) {
+    List<Map<String, dynamic>> temps = [];
+    var i = 0;
+
+    print(i);
+    // ignore: unused_local_variable
+    for (var data in datajson) {
+      temps.add(datajson[i]);
+      i++;
+    }
+    return temps;
+  }
+
+  _initializeData() async {
+    _mockPullData();
+  }
+
+  _mockPullData() async {
+    _expanded = List.generate(_currentPerPage!, (index) => false);
+
+    setState(() => _isLoading = true);
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      _sourceOriginal.clear();
+      _sourceOriginal.addAll(_generateData(n: random.nextInt(10000)));
+      _sourceFiltered = _sourceOriginal;
+      _total = _sourceFiltered.length;
+      _source = _currentPerPage! > datajson.length
+          ? _sourceFiltered.getRange(0, datajson.length).toList()
+          : _sourceFiltered.getRange(0, _currentPerPage!).toList();
+      setState(() => _isLoading = false);
+    });
+  }
+
+  _resetData({start: 0}) async {
+    setState(() => _isLoading = true);
+    var _expandedLen =
+        _total! - start < _currentPerPage! ? _total! - start : _currentPerPage;
+    Future.delayed(Duration(seconds: 0)).then((value) {
+      _expanded = List.generate(_expandedLen as int, (index) => false);
+      _source.clear();
+      _source = _sourceFiltered.getRange(start, start + _expandedLen).toList();
+      setState(() => _isLoading = false);
+    });
+  }
+
+  _filterData(value) {
+    setState(() => _isLoading = true);
+
+    try {
+      if (value == "" || value == null) {
+        _sourceFiltered = _sourceOriginal;
+      } else {
+        _sourceFiltered = _sourceOriginal
+            .where((data) => data[_searchKey!]
+                .toString()
+                .toLowerCase()
+                .contains(value.toString().toLowerCase()))
+            .toList();
+      }
+
+      _total = _sourceFiltered.length;
+      var _rangeTop = _total! < _currentPerPage! ? _total : _currentPerPage!;
+      _expanded = List.generate(_rangeTop!, (index) => false);
+      _source = _sourceFiltered.getRange(0, _rangeTop).toList();
+    } catch (e) {
+      print(e);
+    }
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  void initState() {
+    _total = datajson.length;
+    super.initState();
+    _headers = [];
+
+    /// set headers
+    for (int i = 0; i < datajson[0].keys.toList().length; i++) {
+      _headers.add(DatatableHeader(
+          text: datajson[0].keys.toList()[i].toString(),
+          value: datajson[0].keys.toList()[i].toString(),
+          show: true,
+          sortable: true,
+          textAlign: TextAlign.center));
+    }
+
+    _initializeData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DATA TABLE"),
+        actions: [
+          IconButton(
+            onPressed: _initializeData,
+            icon: Icon(Icons.refresh_sharp),
+          ),
+        ],
+      ),
+      drawer: kullanici(),
+      body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(0),
+              constraints: BoxConstraints(
+                maxHeight: 700,
+              ),
+              child: Card(
+                elevation: 1,
+                shadowColor: Colors.black,
+                clipBehavior: Clip.none,
+                child: ResponsiveDatatable(
+                  reponseScreenSizes: [ScreenSize.xs],
+                  actions: [
+                    if (_isSearch)
+                      Expanded(
+                          child: TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter search term based on ' +
+                                _searchKey!
+                                    .replaceAll(new RegExp('[\\W_]+'), ' ')
+                                    .toUpperCase(),
+                            prefixIcon: IconButton(
+                                icon: Icon(Icons.cancel),
+                                onPressed: () {
+                                  setState(() {
+                                    _isSearch = false;
+                                  });
+                                  _initializeData();
+                                }),
+                            suffixIcon: IconButton(
+                                icon: Icon(Icons.search), onPressed: () {})),
+                        onSubmitted: (value) {
+                          _filterData(value);
+                        },
+                      )),
+                    if (!_isSearch)
+                      IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () {
+                            setState(() {
+                              _isSearch = true;
+                            });
+                          })
+                  ],
+                  headers: _headers,
+                  source: _source,
+                  selecteds: _selecteds,
+                  autoHeight: false,
+                  dropContainer: (data) {
+                    return _DropDownContainer(data: data);
+                  },
+                  onChangedRow: (value, header) {
+                    /// print(value);
+                    /// print(header);
+                  },
+                  onSubmittedRow: (value, header) {
+                    /// print(value);
+                    /// print(header);
+                  },
+                  onTabRow: (data) {
+                    print(data);
+                  },
+                  onSort: (value) {
+                    setState(() => _isLoading = true);
+
+                    setState(() {
+                      _sortColumn = value;
+                      _sortAscending = !_sortAscending;
+                      if (_sortAscending) {
+                        _sourceFiltered.sort((a, b) =>
+                            b["$_sortColumn"].compareTo(a["$_sortColumn"]));
+                      } else {
+                        _sourceFiltered.sort((a, b) =>
+                            a["$_sortColumn"].compareTo(b["$_sortColumn"]));
+                      }
+                      var _rangeTop = _currentPerPage! < _sourceFiltered.length
+                          ? _currentPerPage!
+                          : _sourceFiltered.length;
+                      _source = _sourceFiltered.getRange(0, _rangeTop).toList();
+                      _searchKey = value;
+
+                      _isLoading = false;
+                    });
+                  },
+                  expanded: _expanded,
+                  sortAscending: _sortAscending,
+                  sortColumn: _sortColumn,
+                  isLoading: _isLoading,
+                  footers: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Text("Rows per page:"),
+                    ),
+                    if (_perPages.isNotEmpty)
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: DropdownButton<int>(
+                          value: _currentPerPage,
+                          items: _perPages
+                              .map((e) => DropdownMenuItem<int>(
+                                    child: Text("$e"),
+                                    value: e,
+                                  ))
+                              .toList(),
+                          onChanged: (dynamic value) {
+                            setState(() {
+                              _currentPerPage = value;
+                              _currentPage = 1;
+                              _resetData();
+                            });
+                          },
+                          isExpanded: false,
+                        ),
+                      ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child:
+                          Text("$_currentPage - $_currentPerPage of $_total"),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        size: 16,
+                      ),
+                      onPressed: _currentPage == 1
+                          ? null
+                          : () {
+                              var _nextSet = _currentPage - _currentPerPage!;
+                              setState(() {
+                                _currentPage = _nextSet > 1 ? _nextSet : 1;
+                                _resetData(start: _currentPage - 1);
+                              });
+                            },
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward_ios, size: 16),
+                      onPressed: _currentPage + _currentPerPage! - 1 > _total!
+                          ? null
+                          : () {
+                              var _nextSet = _currentPage + _currentPerPage!;
+
+                              setState(() {
+                                _currentPage = _nextSet < _total!
+                                    ? _nextSet
+                                    : _total! - _currentPerPage!;
+                                _resetData(start: _nextSet - 1);
+                              });
+                            },
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                    )
+                  ],
+                  headerDecoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border(
+                          bottom: BorderSide(color: Colors.red, width: 1))),
+                  selectedDecoration: BoxDecoration(
+                    border: Border(
+                        bottom:
+                            BorderSide(color: Colors.green[300]!, width: 1)),
+                    color: Colors.green,
+                  ),
+                  headerTextStyle: TextStyle(color: Colors.white),
+                  rowTextStyle: TextStyle(color: Colors.green),
+                  selectedTextStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ])),
+    );
+  }
+}
+
+class _DropDownContainer extends StatelessWidget {
+  final Map<String, dynamic> data;
+  const _DropDownContainer({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _children = data.entries.map<Widget>((entry) {
+      Widget w = Row(
+        children: [
+          Text(entry.key.toString()),
+          Spacer(),
+          Text(entry.value.toString()),
+        ],
+      );
+      return w;
+    }).toList();
+
+    return Container(
+      /// height: 100,
+      child: Column(
+        /// children: [
+        ///   Expanded(
+        ///       child: Container(
+        ///     color: Colors.red,
+        ///     height: 50,
+        ///   )),
+        /// ],
+        children: _children,
+      ),
+    );
+  }
+}
