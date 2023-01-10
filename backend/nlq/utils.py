5154a60bb03211576974dc56654ee4a4378b25ac
canaybozkus <bozkuscanay@gmail.com>
2023-01-10 22:34:08 +0300

    database connection requirements

--- backend/nlq/utils.py
+++ backend/nlq/utils.py
@@ -9,13 +9,13 @@ def connect_sql_db(db_type, username, password, server, db):
     connection_string = f"://{username}:{password}@{server}/{db}"
 
     if db_type == 'mysql':
-        connection_string = "mysql+mysqldb" + connection_string
+        connection_string = "mysql+pymysql" + connection_string
 
     elif db_type == 'postgresql':
         connection_string = "postgresql+psycopg2" + connection_string
 
     elif db_type == 'mssql':
-        connection_string = "mssql+pyodbc" + connection_string
+        connection_string = "mssql+pymssql" + connection_string
     elif db_type == 'oracle':
         connection_string = "oracle+cx_oracle" + connection_string
 
