d09092ffaf40b67dc73e450082c2a47e05a1adcb
canaybozkus <bozkuscanay@gmail.com>
2023-05-16 19:39:38 +0300

    dockerize

--- backend/dockerize.sh
+++ backend/dockerize.sh
@@ -2,10 +2,12 @@
 
 #pip3 freeze > requirements.txt
 
-docker rmi -f $(docker images 'nql_django' -a -q)
-
+docker rmi -f $(docker images 'nlq_django' -a -q)
+docker stop nlq
+docker rm nlq
 #rm nql_django.tar
 
-docker build -t nql_django .
+docker build -t nlq_django .
+docker run --name nlq -p 8000:8000 -d nlq_django
 
 #docker save nql_django > nql_django.tar
