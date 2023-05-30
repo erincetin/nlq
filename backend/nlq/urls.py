35e83e969945c4c6b9da47bdb5029211a968afcd
canaybozkus <bozkuscanay@gmail.com>
2023-05-30 22:03:11 +0300

    urls

--- backend/nlq/urls.py
+++ backend/nlq/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('get-sql-query', views.get_sql_query),
     path('register-database', views.database_info),
-    path('get-query-data', views.get_query_result)
+    path('get-query-data', views.get_query_result),
+    path('get-nosql-query', views.get_nosql_query),
+    path('get-nosql-query-data', views.get_nosql_query_result),
 ]
